552404153a4f201c8745b4e3f08930e5e78ffc0e
Chris <cbrosnan00@gmail.com>
2024-09-16 08:50:20 +0100

    Update PokemonProfile.tsx

--- resources/js/Components/PokemonProfile.tsx
+++ resources/js/Components/PokemonProfile.tsx
@@ -52,7 +52,7 @@ function PokemonProfile({ apiUrl, name }: { apiUrl: string, name: string }) {
     const [latest_cry, getLatestCry] = useState('');
 
     useEffect(() => {
-        axios.get(apiUrl + `/pokemon/name/` + name)
+        axios.get(`https://blissful-goodall.18-135-101-14.plesk.page/api/pokemon/name/` + name)
             .then(response => {
                 getPokemon(response.data);
                 getLatestCry(response.data.sound);
